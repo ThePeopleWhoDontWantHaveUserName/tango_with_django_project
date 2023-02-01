f8333fbe326d3d497f5aeff26395bb702910df0f
fu shaopei <2721871f@student.gla.ac.uk>
2023-02-01 18:36:34 +0000

    chapter 4 done

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
-    path('',views.index, name = 'index'),
+    path('',views.index,name='index'),
+    path('index/',views.index, name = 'index'),
     path('rango/', include("rango.urls")),
     path('about/',views.about,name='about'),
     path('about/',include("rango.urls")),
     path('admin/', admin.site.urls),
     
-]
+]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
